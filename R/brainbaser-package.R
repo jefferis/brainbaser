2acfb1473d5fecef9898af9625016c6344ace065
Gregory Jefferis <jefferis@gmail.com>
2015-08-17 16:28:24 +0100

    basic package docs

---
+++ R/brainbaser-package.R
@@ -0,0 +1,20 @@
+#' Programmatic Access to the Brainbase Drosophila Neuroanatomy Website
+#'
+#' This package provides programmatic access to some functions of the Brainbase
+#' Drosophila neuroanatomy website, \url{brainbase.imp.ac.at}. In the first
+#' version of the package functionality is focussed on overlap scores of
+#' expression patterns for different neuropil domains.
+#' @name brainbaser-package
+#' @aliases brainbaser
+#' @docType package
+#' @keywords package
+#' @seealso \code{\link{neuropil_overlaps}}, \code{\link{neuropils}}
+#' @references The Vienna Tiles (VT) driver collection (but not its expression
+#' patterns in the adult brain) has been published in:
+#'
+#' Genome-scale functional characterization of Drosophila developmental
+#' enhancers in vivo. Kvon EZ, Kazmar T, Stampfel G, Yanez-Cuna JO, Pagani M,
+#' Schernhuber K, Dickson BJ, Stark A. Nature. 2014 Aug 7;512(7512):91-5
+#' \href{http://dx.doi.org/10.1038/nature13395}{doi:10.1038/nature13395}
+#'
+NULL
